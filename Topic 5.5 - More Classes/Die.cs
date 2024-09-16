efb9a122acf024ec1e3efc8e40df719a1b4c2e6d
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-16 09:39:33 -0400

    Class ended

---
+++ Topic 5.5 - More Classes/Die.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5___More_Classes
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next();
+        }
+    }
+}
